73340eff5bb5f4634eccc129c1e3ce353d533ecf
Kevin Poelstra <kevin.plstr@gmail.com>
2021-06-10 15:23:05 +0200

    start with jwt

---
+++ src/main/java/jwt/JWTController.java
@@ -0,0 +1,9 @@
+package jwt;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JWTController {
+    @RequestMapping({"api/login"})
+    public void login
+}
