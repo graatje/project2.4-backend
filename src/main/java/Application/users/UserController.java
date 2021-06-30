50e49d81c2ccfa5b5d6aed4315aacf7339530f22
Kevin Poelstra <kevin.plstr@gmail.com>
2021-06-30 14:57:26 +0200

    added backend for registering

---
+++ src/main/java/Application/users/UserController.java
@@ -0,0 +1,47 @@
+package Application.users;
+
+import net.minidev.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@RestController
+public class UserController {
+    private UserRepository userRepository;
+    UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("api/register")
+    public ResponseEntity register(@RequestParam("username") String username, @RequestParam("password") String password,
+                                   @RequestParam("email") String email){
+        JSONObject jsonResponse = new JSONObject();
+        if(!this.isEmail(email)) {
+            jsonResponse.put("message", "e-mail is invalid!");
+            return new ResponseEntity(jsonResponse, HttpStatus.BAD_REQUEST);
+
+        }else if(userRepository.findByName(username) != null){
+            jsonResponse.put("message", "user with this username already exists!");
+            return new ResponseEntity(jsonResponse, HttpStatus.CONFLICT);
+        }else if(userRepository.findByEmail(email) != null){
+            jsonResponse.put("message", "user with this email already exists!");
+            return new ResponseEntity(jsonResponse, HttpStatus.CONFLICT);
+        }else{
+            userRepository.save(new User(username, password, email));
+            jsonResponse.put("message", "successfully registered!");
+            return new ResponseEntity(jsonResponse, HttpStatus.OK);
+        }
+    }
+    private boolean isEmail(String text){
+        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.matches();
+    }
+}
