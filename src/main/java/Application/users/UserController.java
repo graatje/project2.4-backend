bc5eb1ce9970c5a8c7e0deeada08895ffdf7536b
mlandHanze <81427710+mlandHanze@users.noreply.github.com>
2021-07-02 11:59:41 +0200

    some validation thingies

--- src/main/java/Application/users/UserController.java
+++ src/main/java/Application/users/UserController.java
@@ -26,7 +26,19 @@ public class UserController {
             jsonResponse.put("message", "e-mail is ongeldig!");
             return new ResponseEntity(jsonResponse, HttpStatus.BAD_REQUEST);
 
-        }else if(userRepository.findByName(username) != null){
+        }else if(!this.cleanUsername(username)){
+            jsonResponse.put("message", "Gebruik a.u.b. alleen letters en cijfers voor gebruikersnaam.")  ;
+            return new ResponseEntity(jsonResponse, HttpStatus.BAD_REQUEST);
+        }
+        else if (username.length() < 3){
+            jsonResponse.put("message", "Gebruikersnaam moet minimaal 3 karakters zijn!");
+            return new ResponseEntity(jsonResponse, HttpStatus.BAD_REQUEST);
+        }
+        else if (password.length() < 8){
+            jsonResponse.put("message", "Wachtwoord moet minimaal 8 karakters zijn!");
+            return new ResponseEntity(jsonResponse, HttpStatus.BAD_REQUEST);
+        }
+        else if(userRepository.findByName(username) != null){
             jsonResponse.put("message", "gebruiker met deze naam bestaat al!");
             return new ResponseEntity(jsonResponse, HttpStatus.CONFLICT);
         }else if(userRepository.findByEmail(email) != null){
@@ -44,5 +56,12 @@ public class UserController {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         return matcher.matches();
+    }
+
+    private boolean cleanUsername(String username){
+        String regex = "^[a-zA-Z0-9_]+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(username);
+        return m.matches();
     }
 }
